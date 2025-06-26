d28f99edd8ad45656931bc2c11a4e70c323110d7
ali-jin <alina.jin.contact@gmail.com>
2025-06-26 10:43:17 +0200

    add getter setter for password

---
+++ 0x02-dart-classes/4-mutables.dart
@@ -0,0 +1,32 @@
+class Password {
+ String _password = "";
+
+ Password({required String password}) {
+  _password = password;
+ }
+
+ bool isValid() {
+  if (_password.length > 16 || _password.length < 8) {
+    return false;
+  }
+
+  bool hasUppercase = _password.contains(new RegExp(r'[A-Z]'));
+  bool hasDigits = _password.contains(new RegExp(r'[0-9]'));
+  bool hasLowercase = _password.contains(new RegExp(r'[a-z]'));
+
+  return hasUppercase & hasLowercase & hasDigits;
+ }
+
+ @override
+  String toString() {
+    return "Your Password is: ${_password}";
+  }
+
+  String get password {
+    return password;
+  }
+
+  void set password(String password) {
+    this._password = password;
+  }
+}
