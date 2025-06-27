3653aa6e91670e4c8f2209d93444efd46fbbed0f
ali-jin <alina.jin.contact@gmail.com>
2025-06-27 15:00:40 +0200

    update the function

--- 0x03-asynchronous/4-get_sum.dart
+++ 0x03-asynchronous/4-get_sum.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import '4-util.dart';
 
-Future<double> calculateTotal() async {
+calculateTotal() async {
   try {
     final userData = await fetchUserData();
     final user = jsonDecode(userData);
